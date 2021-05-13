bfe3e80061d2c5f92dbe8266166953ee7cec5862
SulGS <saulcaballeroluca@gmail.com>
2021-05-13 18:35:01 +0200

    Initial Commit

---
+++ src/main/java/com/sulgames/commapsy/CommapsApiApplication.java
@@ -0,0 +1,13 @@
+package com.sulgames.commapsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommapsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommapsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sulgames/commapsy/CommapsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sulgames.commapsy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommapsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
